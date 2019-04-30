485d2f3380f55f07e430c47f642b989ca9c51b8e
MasterOfKarats <47531067+MasterOfKarats@users.noreply.github.com>
2019-04-30 12:22:56 -0400

    Initial commit

---
+++ src/GameSnake/model/Physics/World.scala
@@ -0,0 +1,8 @@
+package GameSnake.model.Physics
+
+import GameSnake.model.objects.{ScreenBound, Snake}
+class World {
+  var playerOn: List[Snake] = List()
+  var Boundry: ScreenBound = new ScreenBound(400,400)
+
+}
---
+++ src/GameSnake/web/Static/game.js
@@ -0,0 +1,13 @@
+
+
+function gameState(event) {
+    var state = JSON.parse(event)
+    drawCanvas(state['gridSize'])
+
+}
+function drawCanvas(size) {
+
+}
+function drawCircle(x, y, size, color) {
+    
+}
